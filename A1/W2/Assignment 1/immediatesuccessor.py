b78998b2a8947f16ed47eb61c16ea3618722197c
ItsDanny <danny.desnoo@ddesnoo.nl>
2024-02-14 01:47:52 +0100

    Commit 14-02-2023
    Made changes to W2-Problem 8
    And redone W2-Assignment 1 to make it compliant with the no import datetime rule of CodeGrade

--- A1/W2/Assignment 1/immediatesuccessor.py
+++ A1/W2/Assignment 1/immediatesuccessor.py
@@ -1,2 +1,91 @@
 __author__ = 'Danny de Snoo, HR-nr: 1091749'
 
+# Define a list of invalid characters
+def_invalid_characters = ["/", "_", ".", ",", "|"]
+
+# Request an input from the user
+input_date = input("Input Date: ")
+
+# Check if the string contains any characters that are not allowed
+if any(x in input_date for x in def_invalid_characters):
+    print("Error: Correct Format: YYYY-MM-DD")
+else:
+    # If there aren't any foreign characters in the string continue preforming the check
+
+    # Check if the input can be split into 3 parts
+    if len(input_date.split("-")) == 3:
+        # If it can be split, continue the check
+        separated_input_date = input_date.split("-")
+
+        # Check if all the separated parts are the correct length
+        date_year = int(separated_input_date[0])
+        date_month = int(separated_input_date[1])
+        date_day = int(separated_input_date[2])
+
+        # Check if the year is the correct length
+        if len(str(date_year)) == 4:
+            # Check if the month is a valid month
+            if 0 < int(date_month) <= 12:
+                if date_month == 2:
+                    # Check if February doesn't have a value greater than 28, if so give an error message
+                    if int(date_day) > 28:
+                        print("Error: Correct Format: YYYY-MM-DD")
+                    # Check if the day given is the last day of February
+                    elif int(date_day) == 28:
+                        # If so set the month to next month and set the day to the first day of the month
+                        date_month = 3
+                        date_day = 1
+                        print(f"Next Date: {date_year}-{date_month}-{date_day}")
+                    else:
+                        # Else add 1 day
+                        date_day += 1
+                        print(f"Next Date: {date_year}-{date_month}-{date_day}")
+                elif int(date_month) == 12:
+                    # Check if the day is the last day of the month December
+                    if int(date_day) == 31:
+                        # If so then set the new date to the following year
+                        date_year += 1
+                        date_month = 1
+                        date_day = 1
+                        print(f"Next Date: {date_year}-{date_month}-{date_day}")
+                    else:
+                        # else add 1 day
+                        date_day += 1
+                        print(f"Next Date: {date_year}-{date_month}-{date_day}")
+                else:
+                    # Check if the integer can be divided
+                    if int(date_month) % 2:
+                        # If so then the max date for that month is 30
+                        if int(date_day) > 30:
+                            print("Error: Correct Format: YYYY-MM-DD")
+                        elif int(date_day) == 30:
+                            # If it's the last day of the month, add 1 to the month and
+                            # set the day to the first day of the next month
+                            date_month += 1
+                            date_day = 1
+                            print(f"Next Date: {date_year}-{date_month}-{date_day}")
+                        else:
+                            # else add 1 day
+                            date_day += 1
+                            print(f"Next Date: {date_year}-{date_month}-{date_day}")
+                    else:
+                        # Else the max date for the month is 31
+                        if int(date_day) > 31:
+                            print("Error: Correct Format: YYYY-MM-DD")
+                        elif int(date_day) == 31:
+                            # If it's the last day of the month, add 1 to the month and
+                            # set the day to the first day of the next month
+                            date_month += 1
+                            date_day = 1
+                            print(f"Next Date: {date_year}-{date_month}-{date_day}")
+                        else:
+                            # else add 1 day
+                            date_day += 1
+                            print(f"Next Date: {date_year}-{date_month}-{date_day}")
+            else:
+                print("Error: Correct Format: YYYY-MM-DD")
+        else:
+            print("Error: Correct Format: YYYY-MM-DD")
+    else:
+        # If not end the program
+        print("Error: Correct Format: YYYY-MM-DD")
--- A1/W2/Problem 8/licenseplatevalidator.py
+++ A1/W2/Problem 8/licenseplatevalidator.py
@@ -3,32 +3,54 @@ __author__ = 'Danny de Snoo, HR-nr: 1091749'
 # Request an input from the user
 input_licenseplate = input("License: ")
 
-# Split the input based on the "-"
-splittedinput_licenseplate = input_licenseplate.split("-")
+# Preform a first check to see if the license plate is a valid length
+if len(input_licenseplate) == 8:
+    # Split the input based on the "-"
+    splittedinput_licenseplate = input_licenseplate.split("-")
 
-# Preform a simple check to see if the license plate includes 3 parts (Required for all license plates)
-if len(splittedinput_licenseplate) < 0 or len(splittedinput_licenseplate) > 3:
-    # If the license plate is an invalid length, print a invalid message.
-    print("Invalid")
+    # Preform a simple check to see if the license plate includes 3 parts (Required for all license plates)
+    if len(splittedinput_licenseplate) < 0 or len(splittedinput_licenseplate) > 3:
+        # If the license plate is an invalid length, print an invalid message.
+        print("Invalid")
+    else:
+        # Create 2 variables to see if all parts are valid and to see on which part you currently are
+        invalid_parts = 0
+        part = 0
+
+        # do a for-loop over the license plate parts
+        for i in splittedinput_licenseplate:
+            if len(i) <= 3 and part != 1:
+                # If the license plate part is a valid length and not the second part then continue the check.
+                # Check if it's a full alphabetic part or a full numeric part
+                if i.isalpha():
+                    # Fully alphabetic, continue check
+                    part += 1
+                elif i.isnumeric():
+                    # Fully numeric, continue check
+                    part += 1
+                else:
+                    # If it's not fully an alphabetic or numeric part, Dean the part as invalid and continue check
+                    invalid_parts += 1
+                    part += 1
+            elif part == 1 and len(i) != 1:
+                # If the second part is not 1 character long then continue the check, else dean it as an invalid part
+                if i.isalpha():
+                    part += 1
+                elif i.isnumeric():
+                    part += 1
+                else:
+                    invalid_parts += 1
+                    part += 1
+            else:
+                invalid_parts += 1
+                part += 1
+
+        # Check if there are any invalid parts detect.
+        if invalid_parts == 0:
+            # If not print that its valid
+            print("Valid")
+        else:
+            # If there are print that its invalid
+            print("Invalid")
 else:
-    # If the license plate is a valid length, preform the rest of required checks
-
-    # Split the 3 parts from the list into 3 variables
-    license_p1 = splittedinput_licenseplate[0]
-    license_p2 = splittedinput_licenseplate[1]
-    license_p3 = splittedinput_licenseplate[2]
-
-    # Preform the first check
-    # ---------------------------------------------------------
-
-    # ---------------------------------------------------------
-
-    # Preform the second check
-    # ---------------------------------------------------------
-
-    # ---------------------------------------------------------
-
-    # Preform the third check
-    # ---------------------------------------------------------
-
-    # ---------------------------------------------------------
+    print("Invalid")
