1d5222f2af68cf49a5a8e7c429ad9350ff65d992
ItsDanny <danny.desnoo@ddesnoo.nl>
2024-03-25 19:58:28 +0100

    Add some comments to the code

--- A2/W6/Problem 4/averagetemperatures.py
+++ A2/W6/Problem 4/averagetemperatures.py
@@ -32,57 +32,83 @@ def uniquetemperatures_task1():
     # Create an empty set variable
     unique_temps = set()
 
-    # Loop through the combined_temps list and add all the unique tempratures to the list
+    # Loop through the combined_temps list and add all the unique temperatures to the list
     for temp in combined_temps:
-        unique_temps.add(temp)
+        if temp not in unique_temps:
+            unique_temps.add(temp)
 
     # Return the length of the list
     return len(unique_temps)
 
 
 def uniquetemperatures_task2():
+    # Retriev the info for the certain years
     tempsinfo_1995 = temperatures[0]
     tempsinfo_2020 = temperatures[2]
 
+    # Retrieve all the temperature readings that happened in these years for the month of march
     temps_1995 = tempsinfo_1995[2]
     temps_2020 = tempsinfo_2020[2]
 
+    # Combine the temps into one set
     combined_temps = temps_1995 + temps_2020
+    # Create an empty set
     unique_temps = set()
 
+    # Loop through the temps and add them to the set
     for temp in combined_temps:
-        unique_temps.add(temp)
+        if temp not in unique_temps:
+            unique_temps.add(temp)
 
+    # Return all the unique temps by
     return len(unique_temps)
 
 
 def highesttemperature():
+    # Retriev the info for the certain years
     tempsinfo_1995 = temperatures[0]
     tempsinfo_2010 = temperatures[1]
     tempsinfo_2020 = temperatures[2]
 
+    # Retrieve all the temperature readings that happened in these years for the month of march
     temps_1995 = tempsinfo_1995[2]
     temps_2010 = tempsinfo_2010[2]
     temps_2020 = tempsinfo_2020[2]
 
+    # Create a variable that will store a float that will hold the hottest temperature for the year of 1995
     hottest_temp_1995 = 0.0
+    # Loop through the temps for the year 1995
     for temp in temps_1995:
+        # Turn the temp string into a float
         float_temp = float(temp)
+        # If the temperature is higher than the previous hottest temperature,
+        # then update the hottest temperature variable for 1995
         if float_temp > hottest_temp_1995:
             hottest_temp_1995 = float_temp
 
+    # Create a variable that will store a float that will hold the hottest temperature for the year of 2010
     hottest_temp_2010 = 0.0
+    # Loop through the temps for the year 2010
     for temp in temps_2010:
+        # Turn the temp string into a float
         float_temp = float(temp)
+        # If the temperature is higher than the previous hottest temperature,
+        # then update the hottest temperature variable for 2010
         if float_temp > hottest_temp_2010:
             hottest_temp_2010 = float_temp
 
+    # Create a variable that will store a float that will hold the hottest temperature for the year of 2020
     hottest_temp_2020 = 0.0
+    # Loop through the temps for the year 2020
     for temp in temps_2020:
+        # Turn the temp string into a float
         float_temp = float(temp)
+        # If the temperature is higher than the previous hottest temperature,
+        # then update the hottest temperature variable for 2020
         if float_temp > hottest_temp_2020:
             hottest_temp_2020 = float_temp
 
+    # Check to see which has the hottest temperature and then return that year
     if hottest_temp_1995 > hottest_temp_2010 and hottest_temp_1995 > hottest_temp_2020:
         return "1995"
     elif hottest_temp_2010 > hottest_temp_1995 and hottest_temp_2010 > hottest_temp_2020:
@@ -94,26 +120,32 @@ def highesttemperature():
 
 
 def warmest_march():
+    # Retriev the info for the certain years
     tempsinfo_1995 = temperatures[0]
     tempsinfo_2010 = temperatures[1]
     tempsinfo_2020 = temperatures[2]
 
+    # Retrieve all the temperature readings that happened in these years for the month of march
     temps_1995 = tempsinfo_1995[2]
     temps_2010 = tempsinfo_2010[2]
     temps_2020 = tempsinfo_2020[2]
 
+    # Loop through all the temperature readings for 1995 and add them up in the month_total_1995 variable
     month_total_1995 = 0.0
     for temp in temps_1995:
         month_total_1995 += float(temp)
 
+    # Loop through all the temperature readings for 2010 and add them up in the month_total_2010 variable
     month_total_2010 = 0.0
     for temp in temps_2010:
         month_total_2010 += float(temp)
 
+    # Loop through all the temperature readings for 2020 and add them up in the month_total_2020 variable
     month_total_2020 = 0.0
     for temp in temps_2020:
         month_total_2020 += float(temp)
 
+    # Check to see which has the highest number and then return that year
     if month_total_1995 > month_total_2010 and month_total_1995 > month_total_2020:
         return "1995"
     elif month_total_2010 > month_total_1995 and month_total_2010 > month_total_2020:
--- A2/W6/Problem 5/morsecodetranslator.py
+++ A2/W6/Problem 5/morsecodetranslator.py
@@ -1,3 +1,5 @@
+# Create two dictionaries that contain the translation for characters to morse code
+# and from morse code to standard characters
 to_morse_code_dict = {"a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....",
                       "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "---", "p": ".--.",
                       "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-", "w": ".--", "v": "...-", "x": "-..-",
@@ -16,17 +18,25 @@ def message_to_morse(message):
     for char in message:
         char_list += char.lower()
 
+    # Create an empty string in which we are going to store the morse code translation
     morse_code = ""
+    # Loop through the characters of the message we are going to translate
     for char in char_list:
+        # If the character is a space, put 3 spaces in that location
         if char == " ":
             morse_code += "   "
+        # If the character is in the morse code dictionary
         elif char in to_morse_code_dict:
+            # Retrieve the corresponding translation
             translated_char = to_morse_code_dict[char]
 
+            # If the morse_code string is empty, then only add the translated character to the string
             if morse_code == "":
                 morse_code += translated_char
+            # Else put the character in string with a space in front of it
             else:
                 morse_code += f" {translated_char}"
+        # If it isn't possible to translate character then give a error message
         else:
             morse_code = f"Can't convert char {char} to morse code"
             break
@@ -36,15 +46,22 @@ def message_to_morse(message):
 
 # This function translates a morse code string into a message
 def morse_to_message(morse):
+    # Split the morse code message
     splitted_morse = morse.split(" ")
+    # Create an integer variable to see how many empty strings there are
     emptys = 0
+    # Create an empty string variable that will store our message
     message = ""
+    # Loop through the splitted morse code message
     for part in splitted_morse:
+        # If the part is empty add 1 to the emptys integer
         if part == "":
             emptys += 1
+            # If there are 3 empty's then add a space to the message
             if emptys == 3:
                 message += " "
                 emptys = 0
+        # If the part is in our morse code dictionary then add the translated letter to our message
         if part in from_morse_code_dict:
             translated_char = from_morse_code_dict[part]
             message += translated_char
@@ -52,17 +69,23 @@ def morse_to_message(morse):
     return message
 
 
+# This function will detect if an inputted text is a morse code message or a regular text message
+# and the call the required function to translate it
 def translate_text(text):
     is_morse = False
     for char in text:
+        # If the first charter is a part of a more code then set the is_morse boolean to
+        # true and then break out of the loop
         if char == "-" or char == ".":
             is_morse = True
             break
         elif char in to_morse_code_dict:
             break
 
+    # If it is a morse code message, then translate it from morse code to a regular text message
     if is_morse:
         return morse_to_message(text)
+    # If it is a normal message, then translate it into a morse code message
     else:
         return message_to_morse(text)
 
